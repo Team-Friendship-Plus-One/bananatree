a34ec5f05f0d93a67c51b39bd9e710226a70e266
danlyg <gih.daniel@gmail.com>
2015-08-22 15:56:51 -0700

    added simple validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,6 @@
 class Client < ActiveRecord::Base
+  validates :first_name, :last_name, :location, :bio, :age, :gender, presence: true
+
   has_many :campaign_clients
   has_many :campaigns, :through => :campaign_clients
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  validates :first_name, :last_name, :location, :email, presence: true
+
+  has_secure_password
+  
   has_many :donations
   has_many :campaigns
 end
