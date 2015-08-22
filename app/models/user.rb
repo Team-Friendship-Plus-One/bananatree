f9fa48f7fb8defb552fa95ef88b1307328470c9f
danlyg <gih.daniel@gmail.com>
2015-08-22 16:02:51 -0700

    fixed email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :first_name, :last_name, :location, :email, presence: true
-  validates :email, uniqueness: true, email: true
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
   has_secure_password
   
