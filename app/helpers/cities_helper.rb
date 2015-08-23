946bf971011f7f149b668b9256a37dff6fa5e18f
danlyg <gih.daniel@gmail.com>
2015-08-22 23:06:47 -0700

    added create logic when a new client is created

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -1,2 +1,18 @@
 module CitiesHelper
+
+  def timezoneTime (timezone="Pacific")
+    if timezone == "Pacific"
+      Time.now
+    elsif timezone == "Mountain"
+      Time.now + (60*60*1)
+    elsif timezone == "Central"
+      Time.now + (60*60*2)
+    elsif timezone == "Eastern"
+      Time.now + (60*60*3)      
+    elsif timezone == "Hawaii"
+      Time.now - (60*60*3)
+    elsif timezone == "Alaska"
+      Time.now - (60*60*1)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822234058) do
+ActiveRecord::Schema.define(version: 20150823030740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150822234058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "state"
+    t.string   "timezone"
   end
 
   create_table "clients", force: :cascade do |t|
