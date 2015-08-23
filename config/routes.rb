5996b6d0387333fc2c228a966b0521383e38e3f6
Andrew Patzsch <mrpatzsch@gmail.com>
2015-08-22 19:00:32 -0700

    fix merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-<<<<<<< HEAD
   get '/team' => 'home#team'
-||||||| merged common ancestors
-=======
   devise_for :users
->>>>>>> dev
   resources :campaigns do
     resources :donations
   end
