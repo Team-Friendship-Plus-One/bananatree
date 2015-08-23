8a4c83a6645cd2a0af839d284cdb081a8623dcda
danlyg <gih.daniel@gmail.com>
2015-08-22 18:45:45 -0700

    fixed donations methods and forms

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -10,27 +10,34 @@ class DonationsController < ApplicationController
   # GET /donations/1
   # GET /donations/1.json
   def show
+    @campaign = Campaign.find_by(:id => params[:campaign_id])
   end
 
   # GET /donations/new
   def new
-    @donation = Donations.new
+    @campaign = Campaign.find_by(:id => params[:campaign_id])
+    @donation = Donation.new
   end
 
   # GET /donations/1/edit
   def edit
+    @campaign = Campaign.find_by(:id => params[:campaign_id])
   end
 
   # POST /donations
   # POST /donations.json
   def create
-    @donation = current_user.donation.new(donation_params)
+    @donation = Donation.new(donation_params)
     @donation.campaign_id = params[:campaign_id]
+    if user_signed_in?
+      @donation.user_id = current_user.id
+    end
+    @campaign = Campaign.find_by(:id => params[:campaign_id])
 
     respond_to do |format|
       if @donation.save
-        format.html { redirect_to @donation, notice: 'Donation was successfully created.' }
-        format.json { render :show, status: :created, location: @donation }
+        format.html { redirect_to [@campaign, @donation], notice: 'Donation was successfully created.' }
+        format.json { render :show, status: :created, location: [@campaign, @donation] }
       else
         format.html { render :new }
         format.json { render json: @donation.errors, status: :unprocessable_entity }
@@ -43,8 +50,8 @@ class DonationsController < ApplicationController
   def update
     respond_to do |format|
       if @donation.update(donation_params)
-        format.html { redirect_to @donation, notice: 'Donation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @donation }
+        format.html { redirect_to campaign_donation_path(@donation.campaign_id, @donation), notice: 'Donation was successfully updated.' }
+        format.json { render :show, status: :ok, location: [@campaign, @donation] }
       else
         format.html { render :edit }
         format.json { render json: @donation.errors, status: :unprocessable_entity }
@@ -57,7 +64,7 @@ class DonationsController < ApplicationController
   def destroy
     @donation.destroy
     respond_to do |format|
-      format.html { redirect_to donations_url, notice: 'Donation was successfully destroyed.' }
+      format.html { redirect_to campaign_donations_path, notice: 'Donation was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
