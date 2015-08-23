c6354a4a229a37b8d30be372dd252c20798e9dc8
Christine Lau <chylau.design@gmail.com>
2015-08-22 21:01:04 -0700

    migration for admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823033941) do
+ActiveRecord::Schema.define(version: 20150823035316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20150823033941) do
     t.string   "phone"
     t.boolean  "signed_through_twitter"
     t.string   "image"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
