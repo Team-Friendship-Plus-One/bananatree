8f6b264f86775908c5d7368c451330f8ecc8d549
Andrew Patzsch <mrpatzsch@gmail.com>
2015-08-23 05:12:58 -0700

    donations now being accepted

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,50 +18,18 @@
 //
 
 // $(document).ready(function() {
-//   $("#simplify-payment-form").on("submit", function() {
-//       // Disable the submit button
-//       $("#process-payment-btn").attr("disabled", "disabled");
-//       // Generate a card token & handle the response
-//       SimplifyCommerce.generateToken({
-//           key: "sbpb_N2Y1YWViODEtNGFmYy00NWQzLTlkY2YtMTE2ZjA4ZjNhYjkx",
-//           card: {
-//               number: $("#cc-number").val(),
-//               cvc: $("#cc-cvc").val(),
-//               expMonth: $("#cc-exp-month").val(),
-//               expYear: $("#cc-exp-year").val().subString(2,3)
-//           }
-//       }, simplifyResponseHandler);
-//       // Prevent the form from submitting
-//       return false;
-//   });
+// 	$('#process-payment-btn').on('click', function() {
+// 		var url = $('#simplify-payment-form').attr('action');
+// 		console.log("URL", url);
+// 		var id = $(this).closest(".campaignId");
+// 		id = parseInt(id.attr('id'));
+// 		dataId = {campaginID: id}
+// 		console.log(id);
 
-//   function simplifyResponseHandler(data) {
-//     var $paymentForm = $("#simplify-payment-form");
-//     // Remove all previous errors
-//     $(".error").remove();
-//     // Check for errors
-//     if (data.error) {
-//         // Show any validation errors
-//         if (data.error.code == "validation") {
-//             var fieldErrors = data.error.fieldErrors,
-//                 fieldErrorsLength = fieldErrors.length,
-//                 errorList = "";
-//             for (var i = 0; i < fieldErrorsLength; i++) {
-//                 errorList += "<div class='error'>Field: '" + fieldErrors[i].field +
-//                              "' is invalid - " + fieldErrors[i].message + "</div>";
-//             }
-//             // Display the errors
-//             $paymentForm.after(errorList);
-//         }
-//         // Re-enable the submit button
-//         $("#process-payment-btn").removeAttr("disabled");
-//     } else {
-//         // The token contains id, last4, and card type
-//         var token = data["id"];
-//         // Insert the token into the form so it gets submitted to the server
-//         $paymentForm.append("<input type='hidden' name='simplifyToken' value='" + token + "' />");
-//         // Submit the form to the server
-//         $paymentForm.get(0).submit();
-//     }
-// 	}
+// 		$.ajax({
+// 			url: "/campaigns/pay",
+// 			type: "POST",
+// 			data: JSON.stringify(dataId)
+// 		});
+// 	});
 // });
